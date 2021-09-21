7f48c8eb8bc83cb7b3d0a9f59e6c37adf39956df
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-09-21 12:45:17 +0200

    sanitation done

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
-from typing import Sequence
+from pandas.core.construction import is_empty_data
 import requests
+import pandas as pd
+from requests.api import get
+import re
 
 uniprot = 'https://www.uniprot.org/uniprot/'
 
@@ -12,3 +15,52 @@ def get_protein_sequence(protein):
         return sequence
     else:
         return ''
+
+def get_data_and_remove_unwanted_columns():
+    df = pd.read_csv('UHT milk P036.csv')
+    df.drop('Protein ID', inplace=True, axis=1)
+    df.drop('Unique', inplace=True, axis=1)
+    df.drop('m/z', inplace=True, axis=1)
+    df.drop('Scan', inplace=True, axis=1)
+    df.drop('Source File', inplace=True, axis=1)
+    df.drop('Found By', inplace=True, axis=1)
+    print(df.columns)
+    return df
+
+def clean_peptide(peptide):
+    mod_start_list = [m.start() for m in re.finditer('\(', peptide)]
+    mod_end_list = [m.start() for m in re.finditer('\)', peptide)]
+    clean_peptide = peptide
+    if len(mod_start_list) is not 0:
+        clean_peptide = peptide[0 : mod_start_list[0]]
+        for i in range(len(mod_start_list) - 1):
+            clean_peptide = clean_peptide + peptide[mod_end_list[i] + 1 : mod_start_list[i+1]]
+        clean_peptide = clean_peptide + peptide[mod_end_list[-1] +1 :]
+    if peptide[1] is '.':
+        clean_peptide = clean_peptide[2:]
+    if peptide[len(peptide) - 2] is '.':
+        clean_peptide = clean_peptide[0:len(clean_peptide)-2]
+    return clean_peptide
+
+
+def sanitize_data(df):
+    protein_sequence = ''
+    current_protein = ''
+    for index, row in df.iterrows():
+        peptide = row['Peptide']
+        peptide = clean_peptide(peptide)
+        if row['Length'] is not len(peptide) and row['End'] - row['Start'] is not len(peptide):
+            print('length mismatch')
+            df.drop(index)
+        if(current_protein != row['Protein Accession']):
+            current_protein = row['Protein Accession']
+            protein_sequence = get_protein_sequence(current_protein)
+        #The start and end are 1-indexed
+        protein_sequence_substring = protein_sequence[row['Start'] - 1 : int(row['End'])]
+        if protein_sequence_substring != peptide:
+            print('peptide mismatch')
+            df.drop(index)
+        return df
+
+df = get_data_and_remove_unwanted_columns()
+df = sanitize_data(df)
