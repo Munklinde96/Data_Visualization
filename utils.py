80030589a2834a933fe5f9e13351e3ee22cbe6d2
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-10-12 14:20:59 +0200

    overlap plot changes + abit of changes to the segment plot - Still looks the same

--- utils.py
+++ utils.py
@@ -249,7 +249,8 @@ def get_color_palette_for_modifications ():
     return modification_types_to_color
 
 def get_peptide_segments_and_modifications(data, delta=0.5, _protein="P02666"):
-    """data is a list of tuples on the form (low,hi, [modifications], [modtypes])"""
+    """data is a list of tuples on the form (low,hi, [modifications], [modtypes], [agg intensity])"""
+    # pos = 0.5
     yplaces = [.5+i for i in range(len(data))]
     data = sorted(data, key=lambda x: x[0], reverse=False)
     modification_types_to_color_map = get_color_palette_for_modifications()
@@ -258,11 +259,9 @@ def get_peptide_segments_and_modifications(data, delta=0.5, _protein="P02666"):
     highest_values_for_index = [0]*len(data)
     position_idx = 0 #value to keep track of where we should place next rectangle
     for i in range (len(data)):
-        low, hi, mod_positions, mod_types = data[i]
+        low, hi, mod_positions, mod_types, intensity = data[i]
         low, hi = low-1, hi-1 #convert to zero based index
         position_idx = i 
-        if i == 12:
-            print("")
         for j in range (len(data)):
             if j >= i:
                 break
@@ -278,6 +277,7 @@ def get_peptide_segments_and_modifications(data, delta=0.5, _protein="P02666"):
             for _ms_pos, mod_type in zip(mod_positions, mod_types): #add mass shift color on rectangles if present
                 ms_color = modification_types_to_color_map[mod_type]
                 modifications.append(patches.Rectangle((low+_ms_pos,pos-delta/2.0),1,delta, facecolor= ms_color, edgecolor="black"))
+        # pos = pos + intensity / 2
     #remove 0's from highest_values_for_index
     highest_values_for_index = [x for x in highest_values_for_index if x != 0]
     height = len(highest_values_for_index)
@@ -286,7 +286,6 @@ def get_peptide_segments_and_modifications(data, delta=0.5, _protein="P02666"):
 def plot_peptide_segments(segments_patches, modifications_patches, height, _protein="P02666"):
     fig = plt.figure(figsize=(30,25))
     ax = fig.add_subplot(111)
-    yplaces = [.5+i for i in range(len(segments_patches))]
     ax.set_ylim((0,height))
 
     for rect in segments_patches:
@@ -322,20 +321,26 @@ def preprocess_data_for_peptide_segment_plot(df, _protein="P02666", size=50):
     df["Modification_types"] = df["PTM"].apply(lambda x: x if pd.isnull(x) else [s.strip() for s in x.split(";")])
 
     # this is the main script, note that we have imported pyplot as plt
-    start_end_df = df[["Start", "End", "Protein Accession", "Peptide", 'Position of Mass Shift', 'PTM', 'Modification_types']]
+    start_end_df = df[["Start", "End", "Protein Accession", "Peptide", 'Position of Mass Shift', 'PTM', 'Modification_types', 'Area Sample 1', 'Area Sample 2', 'Area Sample 3', 'Area Sample 4']]
     #only look at values for protein : P02666
     start_end_df = start_end_df[start_end_df["Protein Accession"] == _protein]
     start_end_df.sort_values('Start', inplace=True)
     start_end_df['index1'] = start_end_df.index
+
+    #Aggregate sample intensity, and normalize it
+    start_end_df['Agg Intensity'] = start_end_df['Area Sample 1'] + start_end_df['Area Sample 2'] + start_end_df['Area Sample 3'] + start_end_df['Area Sample 4']
+    start_end_df['Agg Intensity'] = start_end_df['Agg Intensity'] / start_end_df['Agg Intensity'].sum()
+
     #concat index1 and protein accession
     start_end_df['Protein_Accession_idx'] = start_end_df['Protein Accession'] +"_" + start_end_df['index1'].astype(str) 
-    start_end_df["(start,end,pos_ms,mod_types)"] = start_end_df[["Start", "End", 'Position of Mass Shift', 'Modification_types']].apply(tuple, axis=1)
-    start_end_df.drop(["Start", "End", "index1", 'PTM','Modification_types'], axis=1, inplace=True)
+    start_end_df["(start,end,pos_ms,mod_types, agg_intensity)"] = start_end_df[["Start", "End", 'Position of Mass Shift', 'Modification_types', 'Agg Intensity']].apply(tuple, axis=1)
+    start_end_df.drop(["Start", "End", "index1", 'PTM','Modification_types', 'Area Sample 1', 'Area Sample 2', 'Area Sample 3', 'Area Sample 4'], axis=1, inplace=True)
     start_end_df.sort_values('Protein_Accession_idx', inplace=True)
     new = start_end_df.head(size)
+
     # make dictionary with index as keys and (Start,End) as values
     #data = new.groupby("Protein_Accession_idx").apply(lambda x: x["(start,end,peptide,pos_ms)"].tolist())
-    start_end_ms_modtype_list = new['(start,end,pos_ms,mod_types)'].tolist()
+    start_end_ms_modtype_list = new['(start,end,pos_ms,mod_types, agg_intensity)'].tolist()
     return start_end_ms_modtype_list
 
 # get colour palette from y-value distribution
