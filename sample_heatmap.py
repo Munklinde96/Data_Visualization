0d61d37133f68a66249ee432fd3c8e35a1b1d6f8
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-11-27 16:14:41 +0100

    fixed all sorts of bugs

--- sample_heatmap.py
+++ sample_heatmap.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import seaborn as sns
-from refactored_utils import normalize_intensities_by_protein_intensity
+from refactored_utils import normalize_intensities_by_protein_intensity, normalize_intensities
 
 def split_data_in_samples(df, sample_column_id='Area'):
     sample_columns = [col for col in df.columns if sample_column_id in col]
@@ -20,15 +20,11 @@ def split_data_in_samples(df, sample_column_id='Area'):
 def combine_and_aggregate_intensity(dataframes: list, sample_column_id='Area'):
     sample_columns = [col for col in dataframes[0].columns if sample_column_id in col]
     df_collection = []
-    for i in range(len(dataframes)):
-        df = dataframes[i]
-        df["PTM"] = df["PTM"].fillna("Unmodified")
+    for i, df in enumerate(dataframes):
         df = df.copy()
-        # print(df[[sample_columns[i], 'PTM', '#modifications']].head())
         df['#modifications'] = df['#modifications'].fillna(0)
         df['#modifications'] = df['#modifications'].replace(0,1)
         df['Intensity'] = df[sample_columns[i]].divide(df['#modifications'])
-        # print(df[['Intensity', 'PTM', '#modifications']].head())
         df['PTM'] = df['PTM'].str.split(';').str[0]
         df_new = df[['PTM', 'Intensity']]
         df_new = df_new.groupby(['PTM']).sum()
@@ -45,14 +41,10 @@ def create_and_plot_sample_heatmap(df, _protein='P02666'):
     sns.heatmap(data, cmap='viridis')
 
 def get_sample_heatmap_data(df, _protein='P02666', sample_column_id='Area'):
-    sample_columns = [col for col in df.columns if sample_column_id in col]
-    for col in sample_columns:  #make Area samples Nan values 0
-        df[col] = df[col].fillna(0)
-
-    df = normalize_intensities_by_protein_intensity(df)
     df = df[df['Protein Accession'] == _protein]
-    df_list = split_data_in_samples(df)
-    combined = combine_and_aggregate_intensity(df_list).sort_values(by=['PTM'])
+    df = normalize_intensities(df, sample_column_id=sample_column_id)
+    df_list = split_data_in_samples(df, sample_column_id=sample_column_id)
+    combined = combine_and_aggregate_intensity(df_list, sample_column_id=sample_column_id).sort_values(by=['PTM'])
 
     data = pd.pivot_table(data = combined, index = 'PTM', values = 'Intensity', columns='Sample')
     return data
